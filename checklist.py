2a443e0d7caf74d50936d0fa728f47c6cfe343f7
Jacob26502 <57222813+Jacob26502@users.noreply.github.com>
2020-10-09 23:17:19 +0100

    made it actually work

--- checklist.py
+++ checklist.py
@@ -2,8 +2,8 @@ def elist():
     file=open("emojilist.csv", mode="r").read().split("\n")
     ls=[]
     for c,x in enumerate(file):
-        if c=0:
+        if c==0:
             continue
         else:
             ls.append(x.split(","))
-return ls
+    return ls
