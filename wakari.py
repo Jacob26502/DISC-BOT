941d62534a1271fbb63b7dd9e1b00c28751125d2
Jacob26502 <57222813+Jacob26502@users.noreply.github.com>
2020-10-11 02:07:04 +0100

    made into a routine

--- wakari.py
+++ wakari.py
@@ -5,8 +5,9 @@ myToken = '<bEE8b752dcbad37b07509a6eFc0a18F63e8e9faa>'
 head = {'Authorization': "API-Key bEE8b752dcbad37b07509a6eFc0a18F63e8e9faa"}
 
 body = {"JSON"}
-data = {'url': 'https://www.goodsmile.info/en/product/8379/figma+Megumin+Swimsuit+ver.html'}
-def 
+
+def urlshort(urlin):
+    data = {'url': str(urlin)}
     r= requests.post(url, headers=head, json=data)
 
     link=(str(r.json())[str(r.json()).find("short_code")+14:str(r.json()).find("short_code")+18])
